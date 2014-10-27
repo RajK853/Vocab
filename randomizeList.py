30d456d3fb5f7fdad28a6459023ef97b7e27971e
RajK853 <rgk.rana@yahoo.com>
2014-10-27 10:26:45 +0545

    Randomize word list order

--- randomizeList.py
+++ randomizeList.py
@@ -1,10 +1,10 @@
 # This program just shuffles the word list so that the words in each group get randomized.
-# I recommend not to run this program.
+# I recommend to run this program only once as it also resets the userData and score.
 # Run this program only if you want to randomize the list.
 
 import random, time
 
-for text in ["wordList.txt", "synomn.txt", "acronym.txt"]:
+for text in ["wordList.txt", "synonym.txt"]:
     with open(text, "r") as data:
         List = data.readlines()
         random.shuffle(List)
